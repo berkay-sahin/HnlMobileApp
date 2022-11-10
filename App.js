28a0c0be7647e23eb6adb2cb2c20b9af50d0725e
berkay-sahin <berkyshn@hotmail.com>
2022-11-10 17:54:30 +0300

    token control added

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import * as React from 'react'
-import {} from '@react-navigation/native'
-import {Route} from "./src/route/route"
-
-
+import { } from '@react-navigation/native'
+import { Route } from "./src/route/route"
+import {AuthContext} from './src/context/authContext'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SpinnerContext } from './src/context/spinnerContext';
 
 const App = () => {
+
+  const [tkn,setTkn] = React.useState();
+
+  React.useEffect(()=>{
+    AsyncStorage.getItem("token").then((tk)=>{
+     
+      setTkn(tk);
+    }).catch(er=>{
+      console.log("Error: ",er);
+    })
+  },[])
+  
+  const [spinner,setSpinner]=React.useState("none")
+
+  const setLoadSpinner = (value) => {
+    setSpinner(value) 
+   }
+
+  const setToken = (value) => {
+   setTkn(value) 
+  }
   return (
-   <Route/>
+    <SpinnerContext.Provider value={{spinner,setLoadSpinner}}>
+    <AuthContext.Provider value={{tkn,setToken}}>
+      <Route />
+    </AuthContext.Provider>
+    </SpinnerContext.Provider>
 
   );
 };
